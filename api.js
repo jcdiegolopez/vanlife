f619f36fcbe1c65d031d4dc65dfa3a71c932fe49
Diego Lopez <dijolopez@gmail.com>
2023-06-08 11:33:19 -0600

    final

--- api.js
+++ api.js
@@ -1,15 +1,46 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {collection, getDocs, doc, getDoc, getFirestore} from "firebase/firestore/lite"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAK_OhwoIp5sE-s8S4bKSxYavdgGCAqkUg",
+  authDomain: "vanlife-d57a9.firebaseapp.com",
+  projectId: "vanlife-d57a9",
+  storageBucket: "vanlife-d57a9.appspot.com",
+  messagingSenderId: "448501742817",
+  appId: "1:448501742817:web:31f0bc79ab37b324b64ba0",
+  measurementId: "G-FM2QMCQQKM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db,'vans')
+
+
+
+export async function getVans(){
+    const querySnapshot = await getDocs(vansCollectionRef)
+    const dataArr = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    console.log(dataArr)
+    return dataArr
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const vanSnapshot = await getDoc(docRef)
+    return {
+        ...vanSnapshot.data(),
+        id: vanSnapshot.id
     }
-    const data = await res.json()
-    return data.vans
 }
 
 export async function getHostVans(id) {
