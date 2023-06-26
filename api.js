b65aad4cb20d1e432acd2a2eecdc40e627f26a48
Diego Lopez <dijolopez@gmail.com>
2023-06-26 12:13:15 -0600

    Update api.js

--- api.js
+++ api.js
@@ -43,18 +43,14 @@ export async function getVan(id) {
     }
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const querySnapshot = await getDocs(q)
+    const dataArr = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return dataArr
 }
 
 export async function loginUser(creds) {
